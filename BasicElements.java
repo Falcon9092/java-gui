fb5740a02ebaa9b7058424791e8c5422d7981378
Falcon9092 <angello_9092@yahoo.com>
2019-01-08 12:56:09 +0800

    adding JFrame

--- BasicElements.java
+++ BasicElements.java
@@ -1,5 +1,17 @@
+import javax.swing.JFrame;
+
 public class BasicElements{
 	
 	public static void main(String args[]){
+		JFrame myFrame = new JFrame();
 		System.out.println("basic java elements")
+
+		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+
+		myFrame.setVisible(true);
+
+		myFrame.setSize(900, 600);
+
+		System.out.println("basic java elements");
+	}
 }
